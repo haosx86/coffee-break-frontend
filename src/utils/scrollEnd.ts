9fc6d53baec7b45fbc1fc97b9c83d24794821676
Vladimir Terekhov <vladimir.terekhov.128@gmail.com>
2023-06-29 14:58:12 +0400

    fix: remove delay from scrollEnd fn

--- src/utils/scrollEnd.ts
+++ src/utils/scrollEnd.ts
@@ -1,9 +1,5 @@
-const SCROLL_DELAY = 500
-
 export const scrollEnd = async () => {
   if (!document) return
-
-  await new Promise((resolve) => setTimeout(resolve, SCROLL_DELAY))
 
   window.scrollTo({
     top: document.body.scrollHeight,
